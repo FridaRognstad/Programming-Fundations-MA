cc0ca9f18f6fce4942db4ffb5fd38fe175131a73
FridaRognstad <skyfriro@gmail.com>
2020-12-15 12:53:04 +0100

    .

---
+++ js/{Frida-Rognstad}-pf-ma1.js
@@ -0,0 +1,27 @@
+// question 1
+var signedIn = false;
+var age = 10;
+var gender = "boy";
+
+// question 2
+var firstName = "Frida";
+var lastName = "Rognstad";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+//question 3
+console.log("frog")
+var frog;
+console.log(typeof "frog");
+console.log("The type of frog is undefined")
+
+//question 4
+var orderHasShipped = true;
+
+if (orderHasShipped === true) {
+    console.log("The order shipped");
+} else {
+    console.log("The order did not ship");
+}
+
